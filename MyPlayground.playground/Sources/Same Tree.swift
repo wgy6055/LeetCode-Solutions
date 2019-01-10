68fc9b997d3836008c8f2e79fed8c55987292df1
王冠宇 <wangguanyu@mobike.com>
2019-01-10 13:26:27 +0800

    2019.1.10

---
+++ MyPlayground.playground/Sources/Same Tree.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+public func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if p == nil || q == nil {
+        return false
+    }
+    return p?.val == q?.val && isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+}
