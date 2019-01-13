5c380dcc5c3615f9d8204734cacdfb78cced4cc0
王冠宇 <wangguanyu@mobike.com>
2019-01-13 14:16:33 +0800

    2019.1.12

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,16 +1,2 @@
 import Foundation
 
-public func minDepth(_ root: TreeNode?) -> Int {
-    guard let root = root else { return 0 }
-    return helper(root)
-}
-
-private func helper(_ root: TreeNode?) -> Int {
-    guard let root = root else { return Int.max }
-    
-    if root.left == nil && root.right == nil {
-        return 1
-    }
-    
-    return min(helper(root.left), helper(root.right)) + 1
-}
