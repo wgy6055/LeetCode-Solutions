8242a9984db36996d9e86d5b1c76d6246794f7d9
王冠宇 <wangguanyu@mobike.com>
2019-01-19 14:55:49 +0800

    2019.1.19

--- MyPlayground.playground/Sources/Add Two Numbers.swift
+++ MyPlayground.playground/Sources/Add Two Numbers.swift
@@ -1,2 +1,23 @@
 import Foundation
 
+public func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+    var carry = 0, l1 = l1, l2 = l2
+    let head = ListNode(0)
+    var pointer = head
+    
+    while l1 != nil || l2 != nil || carry != 0 {
+        if l1 != nil {
+            carry += l1!.val
+            l1 = l1?.next
+        }
+        if l2 != nil {
+            carry += l2!.val
+            l2 = l2?.next
+        }
+        pointer.next = ListNode(carry % 10)
+        pointer = pointer.next!
+        carry /= 10
+    }
+    
+    return head.next
+}
