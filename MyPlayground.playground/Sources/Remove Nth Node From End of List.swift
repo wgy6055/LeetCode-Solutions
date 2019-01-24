0095d3fcefb549a96765adb3840fd3868236716b
王冠宇 <wangguanyu@mobike.com>
2019-01-24 13:39:36 +0800

    2019.1.24

--- MyPlayground.playground/Sources/Remove Nth Node From End of List.swift
+++ MyPlayground.playground/Sources/Remove Nth Node From End of List.swift
@@ -1,2 +1,16 @@
 import Foundation
 
+public func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+    let dummy = ListNode(0)
+    dummy.next = head
+    var p1: ListNode? = dummy, p2: ListNode? = dummy
+    for _ in 1...n + 1 {
+        p1 = p1?.next
+    }
+    while p1 != nil {
+        p1 = p1?.next
+        p2 = p2?.next
+    }
+    p2?.next = p2?.next?.next
+    return dummy.next
+}
