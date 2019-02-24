8ca0643aa850bcb7db3e3def993b6c65007b1293
王冠宇 <wangguanyu@mobike.com>
2019-02-24 13:59:03 +0800

    2019.2.24

--- MyPlayground.playground/Sources/Combinations.swift
+++ MyPlayground.playground/Sources/Combinations.swift
@@ -1,2 +1,24 @@
 import Foundation
 
+public func combine(_ n: Int, _ k: Int) -> [[Int]] {
+    var res = [[Int]]()
+    guard n >= k else { return res }
+    
+    var current = [Int]()
+    
+    let nums = [Int](1...n)
+    helper(&res, nums, &current, 0, k)
+    return res
+}
+
+fileprivate func helper(_ res: inout [[Int]], _ nums: [Int], _ current: inout [Int], _ index: Int, _ k: Int) {
+    if current.count == k {
+        res.append(current)
+        return
+    }
+    for i in index..<nums.count {
+        current.append(nums[i])
+        helper(&res, nums, &current, i + 1, k)
+        current.removeLast()
+    }
+}
